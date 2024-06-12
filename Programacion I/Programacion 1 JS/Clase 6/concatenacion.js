0169aa7069f82a8d8921a25e839f5eff62c52673
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-06-12 11:19:58 -0300

    Agregando Clase 6 JS

---
+++ Programacion I/Programacion 1 JS/Clase 6/concatenacion.js
@@ -0,0 +1,14 @@
+var nombre = 'Nacho';
+var apellido = 'Villarraza';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Nacho' + ' ' + 'Villarraza';
+console.log(nombreCompleto2);
+
+var juntos = nombre + 219;
+console.log(juntos);
+
+juntos = 78 + 17 + nombre;
+console.log(juntos);
