97e90e8315698d8c5c66dd85c1a82ab28bff4e90
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-06-12 09:25:38 -0300

    Agregando clases 9 y 10

--- Programacion I/Programacion 1 Python/Clase08/Clase8.py
+++ Programacion I/Programacion 1 Python/Clase08/Clase8.py
@@ -1,10 +1,36 @@
 # Ciclos en Python
 # While
 
-contador = 0
+# contador = 0
+#
+# while contador < 100:
+#     print(f"Ejecutando ciclo While: {contador}")
+#     contador += 1
+# else:
+#     print("Fin ciclo While")
 
-while contador < 100:
-    print(f"Ejecutando ciclo While: {contador}")
-    contador += 1
-else:
-    print("Fin ciclo While")
+# Ciclo For
+
+# cadena = "Hello World"
+#
+# for letra in cadena:
+#     print(letra)
+#
+# print("Fin Ciclo For")
+
+# Break
+
+# for letra in "Alemania":
+#     if letra == 'a':
+#         print(f"Letra encontrada: {letra}")
+#         break
+# else:
+#     print("Fin ciclo For")
+
+# Continue
+
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f"Valor: {i}")
+
