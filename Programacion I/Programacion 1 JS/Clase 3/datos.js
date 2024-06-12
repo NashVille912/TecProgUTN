5aadba1ba6c49cc2b8e39d73c62d3b4affd11f41
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-06-12 11:19:33 -0300

    Agregando Clase 3 JS

---
+++ Programacion I/Programacion 1 JS/Clase 3/datos.js
@@ -0,0 +1,18 @@
+// Tipos de datos en Javascript
+
+//String
+var nombre = 'Nacho';
+console.log(nombre);
+
+//Number
+var num = 3000;
+console.log(num);
+
+//Object
+
+var object = {
+  nombre: 'Nacho',
+  apellido: 'Villarraza',
+  telefono: 122344556677,
+};
+console.log(object);
