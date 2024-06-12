1c50a49fac68bf9cffbd2433f54c2dfafc1064ef
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-06-12 11:20:06 -0300

    Agregando Clase 7 JS

---
+++ Programacion I/Programacion 1 JS/Clase 7/mayorDeEdad.js
@@ -0,0 +1,8 @@
+let edad = 20;
+let adulto = 18;
+
+if (edad >= adulto) {
+  console.log('La persona es Mayor de Edad');
+} else {
+  console.log('La persona NO es Mayor de Edad');
+}
---
+++ Programacion I/Programacion 1 JS/Clase 7/parImpar.js
@@ -0,0 +1,7 @@
+let parImpar = 100;
+
+if (parImpar % 2 == 0) {
+  console.log(`El numero ${parImpar} es PAR`);
+} else {
+  console.log(`El numero ${parImpar} es IMPAR`);
+}
