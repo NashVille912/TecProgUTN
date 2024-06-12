2f9d8418821262d033fb7be37b66f2917e41b0e6
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-06-12 11:19:50 -0300

    Agregando Clase 5 JS

---
+++ Programacion I/Programacion 1 JS/Clase 5/datos.js
@@ -0,0 +1,61 @@
+// // Tipos de datos en Javascript
+
+// //String
+// var nombre = 'Nacho';
+// console.log(typeof nombre);
+
+// //Number
+// var num = 3000;
+// console.log(typeof num);
+// num = 1.5;
+// console.log(typeof num);
+
+// //Object
+
+// var object = {
+//   nombre: 'Nacho',
+//   apellido: 'Villarraza',
+//   telefono: 122344556677,
+// };
+// console.log(typeof object);
+
+// //Boolean
+
+// var bandera = true;
+// console.log(bandera);
+
+// //Functions
+
+// function miFuncion(){}
+// console.log(typeof miFuncion);
+
+// //Symbol
+
+// var simbolo = Symbol("Simbolo");
+// console.log(simbolo);
+
+// //Class
+
+// class Persona {
+//   constructor(nombre, apellido){
+//     this.nombre = nombre;
+//     this.apellido = apellido
+//   }
+// }
+// console.log(typeof Persona);
+
+//Undefined
+var x; // dato no definido
+console.log(typeof x);
+
+//Null
+var y = null; // Dato con ausencia de valor
+console.log(typeof y);
+
+//Arrays
+var autos = ['Citroen', 'Audi', 'BMW', 'Ford'];
+console.log(autos);
+
+//Empty String
+var string = '';
+console.log(typeof string);
